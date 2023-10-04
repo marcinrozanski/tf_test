829db38e8939b4d61b9bd01c577656ceb2e2e0a3
Marcin Rozanski <rozanski.marcin@gmail.com>
2023-10-04 10:29:01 +0100

    test bucket name change

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "marcin-tf-test-bucket"
+  bucket = "marcin-tf-test-bucket-2"
 
   tags = {
     Name        = "Marcin test bucket"
-    Environment = "Test"
+    Environment = "Dev"
   }
 }
