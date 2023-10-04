e8e1328244e67e289731164a39974c2af53b4cd6
Marcin Rozanski <rozanski.marcin@gmail.com>
2023-10-04 10:32:17 +0100

    add test bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,8 @@
-# resource "aws_s3_bucket" "example" {
-#   bucket = "marcin-tf-test-bucket-2"
-# 
-#   tags = {
-#     Name        = "Marcin test bucket"
-#     Environment = "Dev"
-#   }
-# }
+resource "aws_s3_bucket" "example" {
+  bucket = "marcin-tf-test-bucket"
+
+  tags = {
+    Name        = "Marcin test bucket"
+    Environment = "Dev"
+  }
+}
